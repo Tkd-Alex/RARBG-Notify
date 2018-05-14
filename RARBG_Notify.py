3c57420372506ccc8e5421bd0c3c03341c772a6c
Alessandro Maggio <alex.tkd.alex@gmail.com>
2018-05-14 14:01:25 +0200

    Edit code. Scraping from RARBG.to web site. Use proxy and search? query. Download file from encoded url. Move hours in array. Use mongodb as database

---
+++ RARBG_Notify.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging, requests, re, time, os, datetime, sqlite3, random, urllib.parse
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from pprint import pprint
+from pymongo import MongoClient
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+TOKEN = open('token.conf', 'r').read().replace("\n", "")
+PROXIES = open('proxy.list', 'r').read().split("\n")
+LINK = "https://rarbg.to/torrents.php?search="
+
+client = MongoClient('localhost', 27017)
+db = client['rarbg-notify']
+
+hours = [9, 11, 15, 18, 20]
+
+logger = logging.getLogger(__name__)
+
+def help(bot, update):    
+    update.message.reply_text('Use /set <name> to insert a new torrent to check\nUse /unset to show the list of your torrent and delete one\nUse /chek to check if there are updates')
+
+def set(bot, update, args, job_queue):
+    if len(args) >= 1:
+        chat_id = update.message.chat_id
+        name = [y for y in [re.sub('[^0-9a-zA-Z]+', '', x.lower()) for x in args] if y]    
+        
+        user = db.users.find_one({"telegramid": chat_id})
+        if user == None:
+            user = {
+                "telegramid": chat_id,
+                "torrentlist": []
+            }
+            db.users.insert_one(user)
+
+        if name in [x['title'] for x in user['torrentlist']]:
+            update.message.reply_text('Torrent is already in list!')
+            return
+
+        for h in hours:
+            job_queue.run_daily(check, datetime.time(h, 00, 00), context=chat_id, name="{}_{}_{}".format(chat_id, name, h))
+        
+        torrent = { "title": name, "originalname": " ".join(args), "lastnotify": [] }
+        db.users.update_one({"_id": user['_id']}, {'$push': {'torrentlist': torrent}} )
+
+        update.message.reply_text('Torrent successfully set!')
+    else:
+        update.message.reply_text('Usage: /set <name>')
+
+def _check(bot, update, job_queue):
+    chat_id = update.message.chat_id
+    job_queue.run_once(check, 1, context=chat_id)
+
+def check(bot, job):
+    user = db.users.find_one({"telegramid": job.context}) 
+    for value in user['torrentlist']:
+        
+        ua = UserAgent()
+        headers = {'User-Agent': ua.random}
+        proxy = random.choice(PROXIES)
+        proxy = {"http": "http://" + proxy, "https": "http://" + proxy}
+
+        r = requests.get(LINK + value['name'].replace(" ", "+"), headers=headers, proxies=proxy)
+        if r.status_code == 200:
+            soup = BeautifulSoup(r.content, 'html.parser')
+            trs = soup.findAll("tr", {"class":"lista2"})
+            for tr in trs:
+                tds = tr.findAll("td")
+                # tds[1] - Title | tds[3] - Size | tds[4] - Seeders | tds[5] - Leechers
+                linktorrent = tds[1].find("a", {"onmouseout":"return nd();"})
+                title = linktorrent['title']
+                if not title in value["lastnotify"]:
+                    torrent = {
+                        "link": "https://rarbg.to" + linktorrent['title'] ,
+                        "id": linktorrent['title'].replace('/torrent/', ""),
+                        "size": tds[3].text,
+                        "seeders": tds[4].text,
+                        "leechers":  tds[5].text
+                    }
+                    if all(ext in re.sub('[^0-9a-zA-Z]+', '', title.lower()) for ext in value["title"]):
+                        description = "Seeders: <b>{}</b> Leechers: <b>{}</b> Size: <b>{}</b>".format(torrent["seeders"], torrent["leechers"], torrent["size"])
+                        bot.send_message(job.context, text="<b>Serie TV Found:</b>\n{}\n<b>Info:</b>\n{}\n<a href='{}'>Link Torrent</a>".format(title.encode("utf-8"), description.encode("utf-8"), torrent['link']), parse_mode="HTML")
+
+                        args = { 'id': torrent['id'], 'f' : "{}-[rarbg.to].torrent".format(title) }
+                        torrentlink = "https://rarbg.to/download.php?{}".format( urllib.parse.urlencode(args) )
+                        filerequest = requests.get(torrentlink, headers=headers, proxies=proxy)
+                        if(filerequest.status_code == 200):
+                            filename = filerequest.headers.get('Content-Disposition').replace('attachment; filename="', "").replace('"', "")
+                            with open(filename, 'wb') as f:
+                                f.write(filerequest.content)    
+                            bot.send_document(job.context, document=open(filename, 'rb'))
+                            os.remove(filename)
+                            value["lastnotify"].append(title)
+
+def unset(bot, update):
+    user = db.users.find_one({"telegramid": update.message.chat_id}) 
+    if user == None or len(user['torrentlist']) == 0:  
+        keyboard = []
+        for value in user['torrentlist']:
+            keyboard.append([InlineKeyboardButton(value["originalname"], callback_data="{}".format(value["title"]) )])
+        
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        update.message.reply_text('Select the torrent to delete:', reply_markup=reply_markup)
+    else: 
+        update.message.reply_text("You haven't a torrent. Use /set <name>")
+
+def button(bot, update, job_queue):
+    query = update.callback_query
+    
+    for h in hours:
+        job = job_queue.get_jobs_by_name("{}_{}_{}".format(update.message.chat_id, query.data, h))
+        job.schedule_removal()
+
+    user = db.users.find_one({"telegramid": update.message.chat_id})
+    for value in user['torrentlist']:
+        if value['title'] == query.data:
+            torrenttitle = value['originalname'] 
+            break
+    
+    db.users.update_one({"_id": user['_id']}, { '$pull': { 'torrentlist': { 'name': query.data} } })
+
+    bot.edit_message_text(text="Torrent delete: {}".format(torrenttitle),
+                          chat_id=query.message.chat_id,
+                          message_id=query.message.message_id)
+
+def error(bot, update, error):
+    logger.warn('update={}, error={}'.format(update, error))
+
+if __name__ == '__main__':
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
+    
+    dp.add_handler(CommandHandler("start", help))
+    dp.add_handler(CommandHandler("help", help))
+    
+    dp.add_handler(CommandHandler('unset', unset))
+    dp.add_handler(CallbackQueryHandler(button, pass_job_queue=True))
+    
+    dp.add_handler(CommandHandler("set", set, pass_args=True, pass_job_queue=True))
+    dp.add_handler(CommandHandler("check", _check, pass_job_queue=True))
+    
+    dp.add_error_handler(error)
+    
+    updater.start_polling()
+    updater.idle()
+
